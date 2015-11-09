79a062a89c90d9e37a4b99a541a58cba67811327
Adrien Rousselet <nibblerz@gmail.com>
2015-11-09 16:35:57 +0100

    Update Advanced Search Form

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
     if($current_page == 'product.php'){
         $query = $db->prepare('SELECT * FROM products WHERE category = :category AND id != :id LIMIT 3');
         $query->bindValue('category', $current_product['category'],PDO::PARAM_INT);
         $query->bindValue('id', $id, PDO::PARAM_INT);
     }else{
-        $query = $db->prepare('SELECT * FROM products ORDER BY rating DESC LIMIT 2');    
+        $query = $db->prepare('SELECT * FROM products ORDER BY rating DESC LIMIT 2');
     }
     $query->execute();
     $rand_products = $query->fetchAll();
@@ -14,14 +14,14 @@
     <p class="lead">Categories</p>
     <div class="list-group">
         <?php foreach($categories as $category){ ?>
-            <a href="search.php?q=<?= ucfirst($category['name']) ?>&category=<?= $category['id'] ?>" class="list-group-item"><?= ucfirst($category['name']) ?></a>
+            <a href="search.php?category=<?= $category['id'] ?>" class="list-group-item"><?= ucfirst($category['name']) ?></a>
         <?php } ?>
-        
+
     </div>
 
     <p class="lead">Featured products</p>
-    <?php foreach ($rand_products as $product) { 
-        
+    <?php foreach ($rand_products as $product) {
+
     include 'partials/thumbnail_common.php';
 
     } ?>
--- search.php
+++ search.php
@@ -1,31 +1,41 @@
 <?php
     require_once 'partials/head.php';
 
-    echo debug($_GET);
-
     $search = !empty($_GET['q']) ? strip_tags($_GET['q']) : '';
-    $category = !empty($_GET['category']) ? intval($_GET['category']) : '';
-    $price = !empty($_GET['price']) ? strip_tags($_GET['price']) : '';
 
+    $category = !empty($_GET['category']) ? intval($_GET['category']) : 0;
+    $category = !empty($_POST['category']) ? intval($_POST['category']) : $category;
+    $price = !empty($_POST['price']) ? strip_tags($_POST['price']) : '';
+
+    //Get min_price and max_price from price input
 	$price_separator_pos = strpos($price, ',');
-	$min_price = !empty($_GET['price']) ? substr($price, 0, $price_separator_pos) : 0;
-	$max_price = !empty($_GET['price']) ? substr($price, $price_separator_pos+1) : 100;
+	$min_price = !empty($_POST['price']) ? substr($price, 0, $price_separator_pos) : 0;
+	$max_price = !empty($_POST['price']) ? substr($price, $price_separator_pos+1) : 200;
 
     if(!empty($_GET)){
-    	$sql = 'SELECT * FROM products WHERE name LIKE :name OR description LIKE :description OR category = :category AND price > :min_price AND price < :max_price';
+    	$sql = 'SELECT * FROM products WHERE (name LIKE :name OR description LIKE :description) AND price >= :min_price AND price <= :max_price';
+
+    	if(!empty($category)){
+    		$sql .= ' AND category = :category';
+    	}
 
         $query = $db->prepare($sql.' ORDER BY date DESC');
 
         $query->bindValue('description', '%'.$search.'%');
         $query->bindValue('name', '%'.$search.'%');
-        $query->bindValue('category', $category, PDO::PARAM_INT);
-        $query->bindValue('min_price', $min_price, PDO::PARAM_INT);
-        $query->bindValue('max_price', $max_price, PDO::PARAM_INT);
+        $query->bindValue('min_price', intval($min_price), PDO::PARAM_INT);
+        $query->bindValue('max_price', intval($max_price), PDO::PARAM_INT);
+
+        if(!empty($category)){
+        	$query->bindValue('category', $category, PDO::PARAM_INT);
+        }
+
         $query->execute();
         $results = $query->fetchAll();
 
         $count = count($results);
     }
+
 
 ?>
 
@@ -37,7 +47,7 @@
 
 				<form class="form-horizontal" method="GET">
 					<div class="input-group">
-						<input type="text" name="q" class="form-control" placeholder="Search..." value="<?= $search ?>">
+						<input type="text" name="q" class="form-control" placeholder="What are you searching for?" value="<?= $search ?>">
 						<span class="input-group-btn">
 							<button class="btn btn-default" type="submit"><span class=" glyphicon glyphicon-search"></span></button>
 						</span>
@@ -50,14 +60,14 @@
 
 				<h1 class="page-header">Filters</h1>
 
-				<form class="search form-inline" method="GET">
+				<form class="search form-inline" method="POST">
 					<div class="form-group">
 						<input type="hidden" name="q" value="<?= $search ?>">
 
 						<label for="category">Category</label>
 						<select id="category" name="category" class="form-control">
 							<option value="" selected disabled>None</option>
-							<?php foreach($categories as $category_info){ 
+							<?php foreach($categories as $category_info){
 								$selected = $category == $category_info['id'] ? ' selected' : '';
 								?>
 								<option value="<?= $category_info['id'] ?>"<?= $selected ?>><?= ucfirst($category_info['name']) ?></option>
@@ -67,7 +77,7 @@
 
 					<div class="form-group">
 						<label for="price">Price</label>
-						0 € <input id="price" name="price" type="text" value="<?= $min_price.','.$max_price ?>" data-slider-min="0" data-slider-max="100" data-slider-step="1" data-slider-value="[0,100]"/> 100 €
+						0 € <input id="price" name="price" type="text" value="<?= $min_price.','.$max_price ?>" data-slider-min="0" data-slider-max="200" data-slider-step="1" data-slider-value="[<?= $min_price ?>,<?= $max_price ?>]"/> 200 €
 					</div>
 
 					<div class="form-group">
@@ -86,20 +96,20 @@
 			</div><!-- /.col-lg-12 -->
 
 			<hr>
-            <?php if(!empty($search)){ ?>
+            <?php if(!empty($_GET)){ ?>
                 <div class="col-lg-12">
-                <h1 class="page-header"><?= $count ?> search results for "<?= $search ?>"</h1>
+                <h1 class="page-header"><?= $count ?> search results<?= !empty($search) ? ' for "'.$search.'"' : '' ?></h1>
             </div>
 
             <?php foreach($results as $product){ ?>
                 <div class="col-lg-3 col-md-4 col-xs-6 thumb">
                    <?php include 'partials/thumbnail_common.php'; ?>
                 </div>
-            <?php } 
+            <?php }
             } ?>
-			
-			
-			
+
+
+
 
 		</div>
 
